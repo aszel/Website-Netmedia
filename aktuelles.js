e484bcbeb76bdfd8c2316b98b55f843222e02d3e
Martin Becker <martin.becker@unbelievable-machine.com>
2015-01-08 20:33:23 +0100

    Added all files from good ol' website

---
+++ aktuelles.js
@@ -0,0 +1,3 @@
+// JavaScript Document
+var aktuelles ="<ul><li>neueste Publikationen eingestellt</li><li>laufende Projekte komplettiert</li><li>englische Version ist aktualisiert</li</ul>";
+document.write(aktuelles)
---
+++ en/aktuelles.js
@@ -0,0 +1,3 @@
+// JavaScript Document
+var aktuelles ="<ul><li>latest publications updated</li><li>latest projects online</li></ul>";
+document.write(aktuelles)
